abd9e9761af35372b25289f6f3470c8d87c036c8
root <root@centos-linux.shared>
2018-08-31 03:42:28 -0400

    Read ENV from DB

--- web/scripts/init.sh
+++ web/scripts/init.sh
@@ -2,6 +2,7 @@
 
 /usr/sbin/httpd
 
+echo "clear_env = no" >> /etc/php-fpm.d/www.conf
 mkdir -p /run/php-fpm
 /usr/sbin/php-fpm --daemonize
 
--- web/www/index.php
+++ web/www/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$link = mysqli_connect('db1', 'env', 'SecurePW123@FromENV');
+$link = mysqli_connect('db1', getenv('DB1_ENV_MYSQL_USERNAME'), getenv('DB1_ENV_MYSQL_PASSWORD'));
 if (!$link) {
     die('Could not connect: ' . mysqli_error());
 }
