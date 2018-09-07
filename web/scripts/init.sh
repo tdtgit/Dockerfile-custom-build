6d3bddc6086519f12245c5c94955e6c2c3795907
root <root@web.local>
2018-09-07 05:04:05 -0400

    VirtualHost autoload

--- web/scripts/init.sh
+++ web/scripts/init.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Change default docker.local hostname to domain
+sed -i "s/docker\.local/$VIRTUAL_HOST/g" /etc/httpd/conf.d/vhosts.conf
+# Start Apache
 /usr/sbin/httpd
 
+# Config PHP so they can get ENV variables
 echo "clear_env = no" >> /etc/php-fpm.d/www.conf
 mkdir -p /run/php-fpm
 /usr/sbin/php-fpm
