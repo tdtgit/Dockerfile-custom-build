a758c4bb00944db7cee78eeaab8d02db803da30e
Anh Tuan <mrsugarvn@gmail.com>
2018-09-20 17:00:00 +0700

    Complete SSL generate

--- web/scripts/init.sh
+++ web/scripts/init.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
-# Change default docker.local hostname to domain
+function setup_ssl(){
+    echo "Setting SSL"
 
-# Generate a self signed certificate if no valid cert exists.
-if [ ! -e /etc/ssl/$VIRTUAL_HOST.key ] && [[ ! -e /etc/ssl/$VIRTUAL_HOST.key ]]; then
+    if [ ! -e /etc/ssl/$VIRTUAL_HOST.key ] && [[ ! -e /etc/ssl/$VIRTUAL_HOST.crt ]]; then
 
-    if [ ! -d /etc/ssl/ ]; then
-        mkdir -p /etc/ssl/
+        if [ ! -d /etc/ssl/ ]; then
+            mkdir -p /etc/ssl/
+        fi
+
+        echo -e "<VirtualHost *:80>\n\tServerName $VIRTUAL_HOST\nRedirect / https://$VIRTUAL_HOST/\n</VirtualHost>" >> /etc/httpd/conf.d/vhosts-force-ssl.conf
+
+        echo "Certificate not found, creating self signed..."
+
+        openssl req -sha256 -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/$VIRTUAL_HOST.key -out /etc/ssl/$VIRTUAL_HOST.crt -subj "/CN=$VIRTUAL_HOST" &> /dev/null
+
+        sed -i "s/*:80/*:443/g" /etc/httpd/conf.d/vhosts.conf
+        sed -i "s/docker\.local/$VIRTUAL_HOST/g" /etc/httpd/conf.d/vhosts.conf
+
+        sed -i '/DocumentRoot/a SSLEngine on\n\tSSLProtocol All -SSLv2 -SSLv3\n\tSSLCertificateFile /etc/ssl/'"$VIRTUAL_HOST"'.crt\n\tSSLCertificateKeyFile /etc/ssl/'"$VIRTUAL_HOST"'.key' /etc/httpd/conf.d/vhosts.conf
+    else
+        echo "Certificate found, using it."
+        sed -i "s/docker\.local:80/$VIRTUAL_HOST:443/g" /etc/httpd/conf.d/vhosts.conf
+        sed -i "s/docker\.local/$VIRTUAL_HOST/g" /etc/httpd/conf.d/vhosts.conf
+        sed -i '/DocumentRoot/a SSLEngine on\n\tSSLCertificateFile /etc/ssl/'"$VIRTUAL_HOST"'.crt\n\tSSLCertificateKeyFile /etc/ssl/'"$VIRTUAL_HOST"'.key' /etc/httpd/conf.d/vhosts.conf
     fi
+}
 
-    echo "Certificate not found, creating self signed..."
-
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/$VIRTUAL_HOST.key -out /etc/ssl/$VIRTUAL_HOST.crt -subj "/C=UK/ST=Warwickshire/L=Leamington/O=OrgName/OU=IT Department/CN=$VIRTUAL_HOST" &> /dev/null
-
-    sed -i "s/docker\.local:80/$VIRTUAL_HOST:443/g" /etc/httpd/conf.d/vhosts.conf
-    # sed -i "s/docker\.local/$VIRTUAL_HOST:443/g" /etc/httpd/conf.d/vhosts.conf
-
-    sed -i '/DocumentRoot/a SSLEngine on\n\tSSLCertificateFile /etc/ssl/'"$VIRTUAL_HOST"'.crt\n\tSSLCertificateKeyFile /etc/ssl/'"$VIRTUAL_HOST"'.key' /etc/httpd/conf.d/vhosts.conf
+if [ -n $SSL ]
+then
+  if [ "$SSL" = "1" ]
+  then
+    setup_ssl
+  fi
 fi
 
 # Start Apache
@@ -26,4 +42,22 @@ chown -R apache: /var/www/html
 # Config PHP so they can get ENV variables
 echo "clear_env = no" >> /etc/php-fpm.d/www.conf
 mkdir -p /run/php-fpm
-/usr/sbin/php-fpm -F -R
+/usr/sbin/php-fpm
+
+if [ -n $SSL ]
+then
+  if [ "$SSL" = "1" ]
+  then
+    yum install -y epel-release
+    cd /etc/yum.repos.d && wget https://repo.codeit.guru/codeit.el`rpm -q --qf "%{VERSION}" $(rpm -q --whatprovides redhat-release)`.repo
+    # When all thing is up
+    sed -i '/tSSLCertificateKeyFile/a SSLOpenSSLConfCmd DHParameters "/etc/ssl/certs/dhparam.pem"' /etc/httpd/conf.d/vhosts.conf
+    
+    openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+    /usr/sbin/httpd -k restart
+  fi
+fi
+
+
+
+/bin/bash
